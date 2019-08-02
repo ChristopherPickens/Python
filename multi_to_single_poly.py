eae04e1d7d9922f583658b39887107d18b6e57f3
Christopher <44245908+ChristopherPickens@users.noreply.github.com>
2019-08-02 11:22:44 +1000

    Update multi_to_single_poly.py
    Added error handling

--- multi_to_single_poly.py
+++ multi_to_single_poly.py
@@ -1,44 +1,44 @@
-import json
-import sys
-
 '''
 GEOJSON FILE STRUCTURE
 {	type
 	features	[{	type
-					properties	{	CUSTOM ENTITY
-								}
+					properties	{	CUSTOM		}
 					geometry	{	type
-									coordinates	[	region		[	shape		[	coordinates
-}				]}				}				]				]				]
+									coordinates	[	region		[	shape		[	coordinates	[
+]]]]}}]}
 
 This script converts multipolygon geojson into polygon geojson, by removing the region from the record, and changing the geometry type to "Polygon"
 '''
 
-# LOAD GEOJSON FILE
-python_obj = json.loads(open(sys.argv[1],"r").read())
-# DEFINE OUTPUT VARIABLE
-new_dict = {"type":"FeatureCollection","features":[]}
+import json
+import os
+import sys
 
 # DEFINE OUTPUT VARIABLE
-output = open("upload_postcodes.geojson","w+")
+new_dict = {"type":"FeatureCollection","features":[]}\
 
-
+# ENSURE SCRIPT IS CALLED CORRECTLY
 if len(sys.argv) < 2:
-	print("Usage: multi_to_single_poly.py input_file [output_file]")
-	
+	print("ERROR - Usage: multi_to_single_poly.py input_file [output_file]")
 	sys.exit(-1)
-elif len(sys.argv) >= 2:
-	print("Running with:", sys.argv[1])
-	print("")
-	python_obj = json.loads(open(sys.argv[1],"r").read())
-	
-	if len(sys.argv) == 3:
+# ENSURE FILE EXISTS
+elif not os.path.isfile(sys.argv[1]):
+	print("ERROR - File does not exist")
+	sys.exit(-1)
+# ENSURE FILE IS JSON LOADABLE
+else:
+	try:
+		python_obj = json.loads(open(sys.argv[1],"r").read())
+	except ValueError as e:
+		print("ERROR - File format: file is not JSON loadable")
+		sys.exit(-1)
+	# DEFINE THE OUTPUT GEOJSON FILE, USE DEFAULT IF NONE DEFINED
+	if len(sys.argv) >= 3:
 		print("Outputting to:", sys.argv[2])
 		output = open(sys.argv[2],"w+")
 	else:
 		print("Outputting to: output.geojson")
 		output = open("output.geojson","w+")
-
 
 # FOR EACH RECORD
 for i in python_obj['features']:
@@ -72,6 +72,5 @@ for i in python_obj['features']:
 						del l[2]
 			# ADD RECORD TO NEW_DICT VARIABLE, REMOVING REGION LAYER, AND SETTING GEOMETRY TYPE TO POLYGON
 			new_dict['features'].append({'type':i['type'],'properties':i['properties'],'geometry':{'type':'Polygon','coordinates':i['geometry']['coordinates'][j]}})
-#
 
 output.write(json.dumps(new_dict))
